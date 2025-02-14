053665bd34418e6c52011f0925195300bf071777
j4yesh <jayeshbadgujar7052@gmail.com>
2025-02-14 16:41:30 +0530

    deploy change 3

--- build.sh
+++ build.sh
@@ -6,6 +6,19 @@ pip install -r requirement.txt
 
 python manage.py collectstatic --no-input
 python manage.py migrate
-if [[ $CREATE_SUPERUSER ]]; then
-  python manage.py createsuperuser --no-input --username "$DJANGO_SUPERUSER_USERNAME" --email "$DJANGO_SUPERUSER_EMAIL"
-fi
+
+gunicorn your_project_name.wsgi:application --bind 0.0.0.0:8000
+
+
+
+# #!/usr/bin/env bash
+# # exit on error
+# set -o errexit
+
+# pip install -r requirement.txt
+
+# python manage.py collectstatic --no-input
+# python manage.py migrate
+# if [[ $CREATE_SUPERUSER ]]; then
+#   python manage.py createsuperuser --no-input --username "$DJANGO_SUPERUSER_USERNAME" --email "$DJANGO_SUPERUSER_EMAIL"
+# fi
